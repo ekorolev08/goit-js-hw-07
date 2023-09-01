7350d1308e0a06e0fa49aec9890a4f06538f41cc
Yevhen Korolov <ekorolev08@gmail.com>
2023-09-01 17:20:36 +0300

    hw-js-07-00

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const container = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+function createMarkup (arr) {
+    return arr.map(({preview, original, description}) => {
+return `<li class= "gallery__item" id="preview">
+<a class="gallery__link" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a>
+</li>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+galleryContainer.addEventListener('click', handlePhoto);
+
+function handlePhoto(event) {
+    if (event.target === event.currentTarget) {
+      return;
+    }
+    event.preventDefault();
+
+    const modal = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`
+    );
+    modal.show();
+};
+
 console.log(galleryItems);
